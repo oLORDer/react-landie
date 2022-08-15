6593335888fc2904c2b110ebc7111aad6f1c597e
oLORDer <justsendmeow@gmail.com>
2022-08-15 15:40:22 +0300

    icons

--- src/data/socials.js
+++ src/data/socials.js
@@ -1,14 +1,12 @@
-// function importAll(r) {
-//   let images = {};
-//   r.keys().map((item, index) => (images[item.replace('./', '')] = r(item)));
-//   return images;
-// }
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => (images[item.replace('./', '')] = r(item)));
+  return images;
+}
 
-// const images = importAll(
-//   require.context('img/svg', false, /\.(png|jpe?g|svg)$/)
-// );
-
-const images = 'hh';
+const images = importAll(
+  require.context('img/svg', false, /\.(png|jpe?g|svg)$/)
+);
 
 const socialSvg = [
   {
